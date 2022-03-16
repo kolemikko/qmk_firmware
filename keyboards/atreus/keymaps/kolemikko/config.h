bcb4d31ce364b2b8c5dba4d213bb44e018e6f89f
kolemikko <mikko.kolehmainen@tuta.io>
2022-03-16 17:46:02 +0000

    Added atreusHW files for now

---
+++ keyboards/atreus/keymaps/kolemikko/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+#undef UNUSED_PINS
+#define PCBDOWN 1
+
+// Pin configuration
+#define MATRIX_ROW_PINS { D0, D1, D2, D3 }
+#define MATRIX_COL_PINS { F6, F5, F4, B7, B6, B5, B4, B3, B2, B1, B0 }
+#define UNUSED_PINS
